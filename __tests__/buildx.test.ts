a0a15756c11bdcd4a1cf746be72c50d0793eb283
CrazyMax <crazy-max@users.noreply.github.com>
2021-09-01 22:22:37 +0200

    Add `metadata` output
    Signed-off-by: CrazyMax <crazy-max@users.noreply.github.com>

--- __tests__/buildx.test.ts
+++ __tests__/buildx.test.ts
@@ -1,6 +1,38 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import * as semver from 'semver';
-import * as buildx from '../src/buildx';
 import * as exec from '@actions/exec';
+import * as buildx from '../src/buildx';
+import * as context from '../src/context';
+
+const tmpNameSync = path.join('/tmp/.docker-bake-jest', '.tmpname-jest').split(path.sep).join(path.posix.sep);
+const metadata = `{
+  "containerimage.config.digest": "sha256:059b68a595b22564a1cbc167af369349fdc2ecc1f7bc092c2235cbf601a795fd",
+  "containerimage.digest": "sha256:b09b9482c72371486bb2c1d2c2a2633ed1d0b8389e12c8d52b9e052725c0c83c"
+}`;
+
+jest.spyOn(context, 'tmpDir').mockImplementation((): string => {
+  const tmpDir = path.join('/tmp/.docker-bake-jest').split(path.sep).join(path.posix.sep);
+  if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir, {recursive: true});
+  }
+  return tmpDir;
+});
+
+jest.spyOn(context, 'tmpNameSync').mockImplementation((): string => {
+  return tmpNameSync;
+});
+
+describe('getMetadata', () => {
+  it('matches', async () => {
+    const metadataFile = await buildx.getMetadataFile();
+    console.log(`metadataFile: ${metadataFile}`);
+    await fs.writeFileSync(metadataFile, metadata);
+    const expected = await buildx.getMetadata();
+    console.log(`metadata: ${expected}`);
+    expect(expected).toEqual(metadata);
+  });
+});
 
 describe('isAvailable', () => {
   const execSpy: jest.SpyInstance = jest.spyOn(exec, 'getExecOutput');
