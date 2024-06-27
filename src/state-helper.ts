b976ed0e115a5a227a038e6b64f89aab0ac2c8d9
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-06-27 14:21:23 +0200

    move summary support check right after build
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- src/state-helper.ts
+++ src/state-helper.ts
@@ -1,15 +1,14 @@
 import * as core from '@actions/core';
 
 import {BakeDefinition} from '@docker/actions-toolkit/lib/types/buildx/bake';
-import {BuilderInfo} from '@docker/actions-toolkit/lib/types/buildx/builder';
 
 import {Inputs, sanitizeInputs} from './context';
 
 export const tmpDir = process.env['STATE_tmpDir'] || '';
 export const inputs = process.env['STATE_inputs'] ? JSON.parse(process.env['STATE_inputs']) : undefined;
-export const builder = process.env['STATE_builder'] ? <BuilderInfo>JSON.parse(process.env['STATE_builder']) : undefined;
 export const bakeDefinition = process.env['STATE_bakeDefinition'] ? <BakeDefinition>JSON.parse(process.env['STATE_bakeDefinition']) : undefined;
 export const buildRefs = process.env['STATE_buildRefs'] ? process.env['STATE_buildRefs'].split(',') : [];
+export const isSummarySupported = !!process.env['STATE_isSummarySupported'];
 
 export function setTmpDir(tmpDir: string) {
   core.saveState('tmpDir', tmpDir);
@@ -19,14 +18,14 @@ export function setInputs(inputs: Inputs) {
   core.saveState('inputs', JSON.stringify(sanitizeInputs(inputs)));
 }
 
-export function setBuilder(builder: BuilderInfo) {
-  core.saveState('builder', JSON.stringify(builder));
-}
-
 export function setBakeDefinition(bakeDefinition: BakeDefinition) {
   core.saveState('bakeDefinition', JSON.stringify(bakeDefinition));
 }
 
 export function setBuildRefs(buildRefs: Array<string>) {
   core.saveState('buildRefs', buildRefs.join(','));
+}
+
+export function setSummarySupported() {
+  core.saveState('isSummarySupported', 'true');
 }
