4060ab2737c335c53574d95d98271da8820747e5
Julian Liebig <julian.liebig@tudock.de>
2026-06-10 18:46:01 +0200

    Split main into multiple functions
    Signed-off-by: Julian Liebig <julian.liebig@tudock.de>

--- subaction/matrix/src/main.ts
+++ subaction/matrix/src/main.ts
@@ -14,57 +14,14 @@ actionsToolkit.run(
 
     let def: BakeDefinition;
     await core.group(`Parsing definition`, async () => {
-      const args = ['buildx', 'bake'];
-      for (const file of files) {
-        args.push('--file', file);
-      }
-      if (target) {
-        args.push(target);
-      }
-      args.push('--print');
-      const res = await exec.getExecOutput('docker', args, {
-        ignoreReturnCode: true,
-        silent: true,
-        cwd: workdir
-      });
-      if (res.stderr.length > 0 && res.exitCode != 0) {
-        throw new Error(res.stderr);
-      }
-      def = JSON.parse(res.stdout.trim());
+      def = await getBakeDefinition(workdir, files, target);
       core.info(JSON.stringify(def, null, 2));
     });
 
     await core.group(`Generating matrix`, async () => {
-      const result: Array<MatrixConfigEntry> = [];
-      for (const targetName of Object.keys(def.target)) {
-        const target = def.target[targetName];
-        const entry: MatrixConfigEntry = {target: targetName};
-        if (fields.length === 0) {
-          result.push({...entry});
-          continue;
-        }
-        let fieldFound = false;
-        Object.keys(target).forEach(field => {
-          if (fields.includes(field)) {
-            fieldFound = true;
-            const value = target[field];
-            if (Array.isArray(value)) {
-              value.forEach(v => {
-                entry[field] = v;
-                result.push({...entry});
-              });
-            } else {
-              entry[field] = value;
-              result.push({...entry});
-            }
-          }
-        });
-        if (!fieldFound) {
-          result.push({...entry});
-        }
-      }
-      core.info(JSON.stringify(result, null, 2));
-      core.setOutput('matrix', JSON.stringify(result));
+      const matrix: Array<MatrixConfigEntry> = generateDefaultMatrix(def, fields);
+      core.info(JSON.stringify(matrix, null, 2));
+      core.setOutput('matrix', JSON.stringify(matrix));
     });
   }
 );
@@ -76,6 +33,58 @@ function getInputList(name: string) {
         .split(/[\r?\n,]+/)
         .filter(x => x !== '')
     : [];
+}
+
+async function getBakeDefinition(workdir: string, files: Array<string>, target: string): Promise<BakeDefinition> {
+  const args = ['buildx', 'bake'];
+  for (const file of files) {
+    args.push('--file', file);
+  }
+  if (target) {
+    args.push(target);
+  }
+  args.push('--print');
+  const res = await exec.getExecOutput('docker', args, {
+    ignoreReturnCode: true,
+    silent: true,
+    cwd: workdir
+  });
+  if (res.stderr.length > 0 && res.exitCode != 0) {
+    throw new Error(res.stderr);
+  }
+  return JSON.parse(res.stdout.trim());
+}
+
+function generateDefaultMatrix(definition: BakeDefinition, fields: Array<string>) {
+  const result: Array<MatrixConfigEntry> = [];
+  for (const targetName of Object.keys(definition.target)) {
+    const target = definition.target[targetName];
+    const entry: MatrixConfigEntry = {target: targetName};
+    if (fields.length === 0) {
+      result.push({...entry});
+      continue;
+    }
+    let fieldFound = false;
+    Object.keys(target).forEach(field => {
+      if (fields.includes(field)) {
+        fieldFound = true;
+        const value = target[field];
+        if (Array.isArray(value)) {
+          value.forEach(v => {
+            entry[field] = v;
+            result.push({...entry});
+          });
+        } else {
+          entry[field] = value;
+          result.push({...entry});
+        }
+      }
+    });
+    if (!fieldFound) {
+      result.push({...entry});
+    }
+  }
+  return result;
 }
 
 type Optional<Type> = {
