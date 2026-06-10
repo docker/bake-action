986a9feacdc50c9cc17a119069cca322159bbe6e
Julian Liebig <julian.liebig@tudock.de>
2026-06-10 21:16:42 +0200

    Generate layered matrix
    Signed-off-by: Julian Liebig <julian.liebig@tudock.de>

--- subaction/matrix/src/main.ts
+++ subaction/matrix/src/main.ts
@@ -4,6 +4,8 @@ import * as actionsToolkit from '@docker/actions-toolkit';
 
 import {BakeDefinition, Target} from '@docker/actions-toolkit/lib/types/buildx/bake.js';
 
+import {DependencyGraph} from './dependency-graph.js';
+
 actionsToolkit.run(
   // main
   async () => {
@@ -19,9 +21,19 @@ actionsToolkit.run(
     });
 
     await core.group(`Generating matrix`, async () => {
-      const matrix: Array<MatrixConfigEntry> = generateDefaultMatrix(def, fields);
+      const matrix = generateDefaultMatrix(def, fields);
       core.info(JSON.stringify(matrix, null, 2));
       core.setOutput('matrix', JSON.stringify(matrix));
+    });
+
+    await core.group(`Generating layered matrix`, async () => {
+      const graph = new DependencyGraph(def);
+      core.info(`Max amount of layers: ${graph.maxDepth}`);
+      core.setOutput('max-layers', graph.maxDepth);
+      const layeredMatrix = generateLayeredMatrix(graph, fields);
+      core.info(JSON.stringify(layeredMatrix, null, 2));
+      core.setOutput('layered-matrix', JSON.stringify(layeredMatrix));
+      core.setOutput('layers', layeredMatrix.length);
     });
   }
 );
@@ -55,34 +67,52 @@ async function getBakeDefinition(workdir: string, files: Array<string>, target: 
   return JSON.parse(res.stdout.trim());
 }
 
-function generateDefaultMatrix(definition: BakeDefinition, fields: Array<string>) {
+function getTargetsWithFields(definition: BakeDefinition, targetName: string, fields: Array<string>): Array<MatrixConfigEntry> {
+  const entry: MatrixConfigEntry = {target: targetName};
+  if (fields.length === 0) {
+    return [{...entry}];
+  }
+  const result: Array<MatrixConfigEntry> = [];
+  const target = definition.target[targetName];
+  let fieldFound = false;
+  Object.keys(target).forEach(field => {
+    if (fields.includes(field)) {
+      fieldFound = true;
+      const value = target[field];
+      if (Array.isArray(value)) {
+        value.forEach(v => {
+          entry[field] = v;
+          result.push({...entry});
+        });
+      } else {
+        entry[field] = value;
+        result.push({...entry});
+      }
+    }
+  });
+  if (!fieldFound) {
+    result.push({...entry});
+  }
+  return result;
+}
+
+function generateDefaultMatrix(definition: BakeDefinition, fields: Array<string>): Array<MatrixConfigEntry> {
   const result: Array<MatrixConfigEntry> = [];
   for (const targetName of Object.keys(definition.target)) {
-    const target = definition.target[targetName];
-    const entry: MatrixConfigEntry = {target: targetName};
-    if (fields.length === 0) {
-      result.push({...entry});
-      continue;
+    result.push(...getTargetsWithFields(definition, targetName, fields));
+  }
+  return result;
+}
+
+function generateLayeredMatrix(graph: DependencyGraph, fields: Array<string>): Array<Array<MatrixConfigEntry>> {
+  const layeredMatrix = graph.getLayeredMatrix();
+  const result: Array<Array<MatrixConfigEntry>> = [];
+  for (const layerMatrix of layeredMatrix) {
+    const layerResult: Array<MatrixConfigEntry> = [];
+    for (const targetName of layerMatrix.sort()) {
+      layerResult.push(...getTargetsWithFields(graph.definition, targetName, fields));
     }
-    let fieldFound = false;
-    Object.keys(target).forEach(field => {
-      if (fields.includes(field)) {
-        fieldFound = true;
-        const value = target[field];
-        if (Array.isArray(value)) {
-          value.forEach(v => {
-            entry[field] = v;
-            result.push({...entry});
-          });
-        } else {
-          entry[field] = value;
-          result.push({...entry});
-        }
-      }
-    });
-    if (!fieldFound) {
-      result.push({...entry});
-    }
+    result.push(layerResult);
   }
   return result;
 }
