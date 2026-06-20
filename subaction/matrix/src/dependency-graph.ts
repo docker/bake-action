6bdeab36221bb40ff00676465c8382789eb163d8
Julian Liebig <julian.liebig@tudock.de>
2026-06-20 20:28:55 +0200

    Optimize dependency graph
    Signed-off-by: Julian Liebig <julian.liebig@tudock.de>

--- subaction/matrix/src/dependency-graph.ts
+++ subaction/matrix/src/dependency-graph.ts
@@ -3,12 +3,12 @@ import {BakeDefinition} from '@docker/actions-toolkit/lib/types/buildx/bake.js';
 export class DependencyGraph {
   readonly definition: BakeDefinition;
   private _maxDepth: number = -1;
-  private edges: Record<string, Set<string>> = {};
+  private predecessors: Record<string, Set<string>> = {};
 
   get maxDepth(): number {
     if (this._maxDepth == -1) {
       const depths: Array<number> = [];
-      for (const targetName in this.edges) {
+      for (const targetName in this.predecessors) {
         depths.push(this.getMaxDepth(targetName));
       }
       this._maxDepth = Math.max(...depths, 0);
@@ -20,7 +20,9 @@ export class DependencyGraph {
   constructor(definition: BakeDefinition) {
     this.definition = definition;
 
-    this.populateEdges();
+    for (const targetName in this.definition.target) {
+      this.predecessors[targetName] = this.getTargetDependencies(targetName);
+    }
   }
 
   private getTargetDependencies(targetName: string): Set<string> {
@@ -38,67 +40,37 @@ export class DependencyGraph {
     return result;
   }
 
-  private populateEdges(): void {
-    for (const targetName in this.definition.target) {
-      this.edges[targetName] = new Set();
-    }
-    for (const targetName in this.definition.target) {
-      this.getTargetDependencies(targetName).forEach(dep => this.edges[dep].add(targetName));
-    }
-  }
-
   private getMaxDepth(targetName: string): number {
     const targets: Array<number> = [];
-    for (const successor of this.edges[targetName]) {
-      targets.push(this.getMaxDepth(successor) + 1);
+    for (const predecessor of this.predecessors[targetName]) {
+      targets.push(this.getMaxDepth(predecessor) + 1);
     }
     return Math.max(...targets, 1);
   }
 
-  private targetDependsOn(targetName: string, other: string): boolean {
-    const deps: Array<string> = [...this.edges[other]];
-    return deps.some(successorName => successorName == targetName || this.targetDependsOn(targetName, successorName));
-  }
-
-  private withoutDependencies(targets: Set<string>): Set<string> {
-    const originalTargets = [...targets];
-    for (const firstTarget of originalTargets) {
-      for (const secondTarget of originalTargets) {
-        if (firstTarget == secondTarget) {
-          continue;
-        }
-        if (this.targetDependsOn(firstTarget, secondTarget)) {
-          targets.delete(firstTarget);
-          return this.withoutDependencies(targets);
-        }
-      }
-    }
-    return targets;
-  }
-
-  private flattenTargets(targets: Array<string>): Array<string> {
-    const result: Array<string> = [];
-    for (const name of targets) {
-      if (name in this.definition.group) {
-        if (name in this.definition.target && this.definition.group[name].targets.every(result.includes)) {
-          continue;
-        }
-        result.push(...this.flattenTargets(this.definition.group[name].targets));
-      } else {
-        result.push(name);
+  private getTargetsWithoutDependencies(): Set<string> {
+    const result = new Set<string>();
+    for (const targetName in this.predecessors) {
+      if (this.predecessors[targetName].size == 0) {
+        result.add(targetName);
       }
     }
     return result;
   }
 
-  private getReadyTargets(finishedTargets: Array<Array<string>>, previousTarget: string): Set<string> {
+  private getReadyTargets(finishedTargets: Array<string>): Set<string> {
+    if (finishedTargets.length == 0) {
+      return this.getTargetsWithoutDependencies();
+    }
+
     const result = new Set<string>();
-    const flatFinishedTargets = finishedTargets.flat();
-    for (const targetName of this.edges[previousTarget]) {
-      const dependencies = [...this.getTargetDependencies(targetName)];
+    for (const targetName in this.predecessors) {
+      if (finishedTargets.includes(targetName)) {
+        continue;
+      }
       let ready = true;
-      for (const dependency of dependencies) {
-        if (!flatFinishedTargets.includes(dependency)) {
+      for (const dependency of this.predecessors[targetName].values()) {
+        if (!finishedTargets.includes(dependency)) {
           ready = false;
           break;
         }
@@ -110,25 +82,13 @@ export class DependencyGraph {
     return result;
   }
 
-  getProcessMatrices(...affectedTargets: Array<string>): Array<Array<string>> {
+  getLayeredMatrix(): Array<Array<string>> {
     const result: Array<Array<string>> = [];
-    let targets: Set<string> = new Set(this.flattenTargets(affectedTargets));
-    targets = this.withoutDependencies(targets);
+    let targets: Set<string> = this.getReadyTargets([]);
     while (targets.size > 0) {
-      const latestTargets = [...targets];
-      result.push(latestTargets);
-      targets.clear();
-      for (const targetName of latestTargets) {
-        if (!(targetName in this.edges)) {
-          continue;
-        }
-        targets = targets.union(this.getReadyTargets(result, targetName));
-      }
+      result.push([...targets]);
+      targets = this.getReadyTargets(result.flat());
     }
     return result;
-  }
-
-  getLayeredMatrix(): Array<Array<string>> {
-    return this.getProcessMatrices(...Object.keys(this.definition.target));
   }
 }
